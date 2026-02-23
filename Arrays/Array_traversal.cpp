d7271bb622dfad2096982a87be6892a127ffe217
AayushRajput006 <aayushsbrajput@gmail.com>
2026-02-23 23:09:08 +0530

    Array_traversal.cpp

--- Arrays/Array_traversal.cpp
+++ Arrays/Array_traversal.cpp
@@ -1,1 +1,24 @@
+// Array Traversal
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int size;
+
+    cout<<"Enter the size of Array : ";
+    cin>>size;
+
+    int arr[size];
+
+    for(int i=0;i<size;i++){
+        cout<<"Enter the element "<<i+1 <<": ";
+        cin>>arr[i];
+    }
+
+    for(int i=0; i<size;i++){
+        cout<<arr[i]<<"\t";
+    }
+
+    return 0;
+}
