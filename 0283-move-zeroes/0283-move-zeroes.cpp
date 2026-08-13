2d0bfed57d6ec3daa5754bc817273d41c42b6548
AayushRajput006 <aayushsbrajput@gmail.com>
2026-08-13 19:41:39 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for(int i = 0; i<nums.size(); i++){
-            for(int j= i+1; j<nums.size(); j++){
-                if(nums[i]==0){
-                    int temp = nums[i];
-                    nums[i]= nums[j];
-                    nums[j] = temp;
+        int left = 0; // pointer for placing non-zero elements
+
+        // Traverse the array with right pointer
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[right] != 0) {
+                // Swap only when left and right are different
+                if (left != right) {
+                    int temp = nums[left];
+                    nums[left] = nums[right];
+                    nums[right] = temp;
                 }
+                left++; // move left pointer forward
             }
         }
-        
     }
 };
